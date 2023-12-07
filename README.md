8c3c93668c3aa6a4480fad91ba6e97e6572d7975
annikavec <annika.veclani@stud.fhgr.ch>
2023-12-07 22:05:52 +0100

    Letzte Anpassungen

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Bachelorarbeit
- Die Seite meiner Bachelorarbeit
+
+ Projektidee
+ Für meine Bachelorarbeit werde ich eine TikTok Kampagne für das TikTok-Kanal von Engadin Tourismus @engadin erstellen. Um die Videos auf eine originellen Art und Weise auf der Website zu präsentieren hatte ich die Idee, sie auf einer Karte darzustellen. Eine der häufigsten Fragen auf den TikTok Kanal ist “WOW, wo ist das?”. Mit dieser Website möchte ich diese Frage interaktiv beantworten.
+ 
+ Website
+ Die Webseite habe ich mit HTML, JavaScript und CSS programmiert. Die Karte habe ich mit Leaflet eingebettet. Für das Projekt habe ich sehr oft ChatGPT als Hilfe gebraucht.
+ Momentan sind auf der Karte nur einige Beispiele zu sehen. Wenn dann die Videos erstellt werden können diese fortgehend aufgelistet werden!
+ 
+ Challenges
+ Die erste und deutlichste Challenge sind die Pop-Up Fenster auf der Karte. Diese funktionieren super bis auf die Videos. D.h. Hashtags und Caption usw. werden angezeigt, aber die Videos nicht. Ich habe das Problem mit mehreren Leute und auch beim Coaching besprochen. Leider habe ich dafür noch keine Lösung gefunden. Eine Lösung wäre, die Videos von TikTok jedes Mal herunterzuladen und dann im Code wieder hochzuladen. Das ist aber sehr aufwändig und dadurch wird die Website auch langsamer. Das ist der Hauptgrund, warum ich mich gegen diese Option entschieden habe und das Projekt momentan so (ohne Videoanzeige) abgeben werden. Ich bin der Meinung, dass diese Möglichkeit passender ist und hoffe, dass ich das Problem bald lösen kann, damit die Videos dann angezeigt werden. Ich vermute, dass es evtl ein Problem mit TikTok und der Verlinkung gibt. Das ist aber noch in Abklärung.
+ Eine weitere Challenge bestand darin, die Karte anzupassen und das Buergermenu hinzubekommen.
+ 
+ Quellen
+ - ChatGPT
+ - Dokumentation Leaflet
--- js/script.js
+++ js/script.js
@@ -10,31 +10,32 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // TikTok-Videos und ihre Standorte
 var tiktokVideos = [
     {
-        lat: 46.705,
-        lng: 9.477,  
-        embedCode: '<blockquote class="tiktok-embed" cite="https://www.tiktok.com/@engadin/video/7294311041964592417" data-video-id="7294311041964592417" style="max-width: 605px;min-width: 325px;" > <section> <a target="_blank" title="@engadin" href="https://www.tiktok.com/@engadin?refer=embed">@engadin</a> Kömmend mit de Annika mit! 👋🏽 <a title="engadin" target="_blank" href="https://www.tiktok.com/tag/engadin?refer=embed">#engadin</a> <a title="schweiz" target="_blank" href="https://www.tiktok.com/tag/schweiz?refer=embed">#schweiz</a> <a title="berge" target="_blank" href="https://www.tiktok.com/tag/berge?refer=embed">#berge</a> <a title="fd" target="_blank" href="https://www.tiktok.com/tag/fd?refer=embed">#fd</a> <a title="fdseite" target="_blank" href="https://www.tiktok.com/tag/fdseite?refer=embed">#fdseite</a> <a title="fürdich" target="_blank" href="https://www.tiktok.com/tag/f%C3%BCrdich?refer=embed">#fürdich</a> <a target="_blank" title="♬ Originalton - Engadin" href="https://www.tiktok.com/music/Originalton-7294311053458197281?refer=embed">♬ Originalton - Engadin</a> </section> </blockquote> <script async src="https://www.tiktok.com/embed.js"></script>',
+        lat: 46.4427,
+        lng: 9.7666,  
+        embedCode: '<blockquote class="tiktok-embed" cite="https://www.tiktok.com/@engadin/video/7305819015388138785" data-video-id="7305819015388138785" style="max-width: 605px;min-width: 325px;" > <section> <a target="_blank" title="@engadin" href="https://www.tiktok.com/@engadin?refer=embed">@engadin</a> So startend mier de Winter 🔥❄️ <a title="rocktherail" target="_blank" href="https://www.tiktok.com/tag/rocktherail?refer=embed">#rocktherail</a> <a title="engadin" target="_blank" href="https://www.tiktok.com/tag/engadin?refer=embed">#engadin</a> <a title="switzerland" target="_blank" href="https://www.tiktok.com/tag/switzerland?refer=embed">#switzerland</a> <a title="freestyle" target="_blank" href="https://www.tiktok.com/tag/freestyle?refer=embed">#freestyle</a> <a title="freeski" target="_blank" href="https://www.tiktok.com/tag/freeski?refer=embed">#freeski</a> <a title="snowboard" target="_blank" href="https://www.tiktok.com/tag/snowboard?refer=embed">#snowboard</a> <a title="fd" target="_blank" href="https://www.tiktok.com/tag/fd?refer=embed">#fd</a> <a target="_blank" title="♬ Originalton - Engadin" href="https://www.tiktok.com/music/Originalton-7305819031339092768?refer=embed">♬ Originalton - Engadin</a> </section> </blockquote> <script async src="https://www.tiktok.com/embed.js"></script>',
     },
 
     {
-        lat: 46.41495832957872, 
-        lng: 9.968388412699891, 
-        embedCode: '<blockquote class="tiktok-embed" cite="https://www.tiktok.com/@engadin/video/7296909946724076833" data-video-id="7296909946724076833" style="max-width: 605px;min-width: 325px;" > <section> <a target="_blank" title="@engadin" href="https://www.tiktok.com/@engadin?refer=embed">@engadin</a> Bauernhof im Engadin 🐄🏔️ &gt;&gt;&gt;&gt;&gt; <a title="engadin" target="_blank" href="https://www.tiktok.com/tag/engadin?refer=embed">#engadin</a> <a title="bauernhof" target="_blank" href="https://www.tiktok.com/tag/bauernhof?refer=embed">#bauernhof</a> <a title="bauer" target="_blank" href="https://www.tiktok.com/tag/bauer?refer=embed">#bauer</a> <a target="_blank" title="♬ original sound - Engadin" href="https://www.tiktok.com/music/original-sound-7296909976926014241?refer=embed">♬ original sound - Engadin</a> </section> </blockquote> <script async src="https://www.tiktok.com/embed.js"></script>',
+        lat: 46.4511, 
+        lng: 9.7958, 
+        embedCode: '<blockquote class="tiktok-embed" cite="https://www.tiktok.com/@engadin/video/7304707400328547616" data-video-id="7304707400328547616" style="max-width: 605px;min-width: 325px;" > <section> <a target="_blank" title="@engadin" href="https://www.tiktok.com/@engadin?refer=embed">@engadin</a> Einkaufsliste für 2 Personen: 450 gr Wildfleisch (Reh, Hirsch, Wildschwein) 1 Becher Creme Fraiche 75 ml Wildfond 1 Lorbeerblatt 3 Wacholderbeeren 2 Gewürznelken 0.5 TL getrockneter Thymian 1 EL Butter 1 Zwiebel 50g Speck 1 EL Mehl   <a title="engadin" target="_blank" href="https://www.tiktok.com/tag/engadin?refer=embed">#engadin</a> <a title="schweiz" target="_blank" href="https://www.tiktok.com/tag/schweiz?refer=embed">#schweiz</a> <a title="wild" target="_blank" href="https://www.tiktok.com/tag/wild?refer=embed">#wild</a> <a target="_blank" title="♬ original sound - Engadin" href="https://www.tiktok.com/music/original-sound-7304707418221857568?refer=embed">♬ original sound - Engadin</a> </section> </blockquote> <script async src="https://www.tiktok.com/embed.js"></script>',
     },
     {
-        lat: 46.49363196627833,  
-        lng: 9.842625475285544, 
-        embedCode: '<blockquote class="tiktok-embed" cite="https://www.tiktok.com/@engadin/video/7296909946724076833" data-video-id="7296909946724076833" style="max-width: 605px;min-width: 325px;" > <section> <a target="_blank" title="@engadin" href="https://www.tiktok.com/@engadin?refer=embed">@engadin</a> Bauernhof im Engadin 🐄🏔️ &gt;&gt;&gt;&gt;&gt; <a title="engadin" target="_blank" href="https://www.tiktok.com/tag/engadin?refer=embed">#engadin</a> <a title="bauernhof" target="_blank" href="https://www.tiktok.com/tag/bauernhof?refer=embed">#bauernhof</a> <a title="bauer" target="_blank" href="https://www.tiktok.com/tag/bauer?refer=embed">#bauer</a> <a target="_blank" title="♬ original sound - Engadin" href="https://www.tiktok.com/music/original-sound-7296909976926014241?refer=embed">♬ original sound - Engadin</a> </section> </blockquote> <script async src="https://www.tiktok.com/embed.js"></script>',
+        lat: 46.5106,  
+        lng: 9.8722, 
+        embedCode: '<blockquote class="tiktok-embed" cite="https://www.tiktok.com/@engadin/video/7294311041964592417" data-video-id="7294311041964592417" style="max-width: 605px;min-width: 325px;" > <section> <a target="_blank" title="@engadin" href="https://www.tiktok.com/@engadin?refer=embed">@engadin</a> Kömmend mit de Annika mit! 👋🏽 <a title="engadin" target="_blank" href="https://www.tiktok.com/tag/engadin?refer=embed">#engadin</a> <a title="schweiz" target="_blank" href="https://www.tiktok.com/tag/schweiz?refer=embed">#schweiz</a> <a title="berge" target="_blank" href="https://www.tiktok.com/tag/berge?refer=embed">#berge</a> <a title="fd" target="_blank" href="https://www.tiktok.com/tag/fd?refer=embed">#fd</a> <a title="fdseite" target="_blank" href="https://www.tiktok.com/tag/fdseite?refer=embed">#fdseite</a> <a title="fürdich" target="_blank" href="https://www.tiktok.com/tag/f%C3%BCrdich?refer=embed">#fürdich</a> <a target="_blank" title="♬ Originalton - Engadin" href="https://www.tiktok.com/music/Originalton-7294311053458197281?refer=embed">♬ Originalton - Engadin</a> </section> </blockquote> <script async src="https://www.tiktok.com/embed.js"></script>',
     },
     {
-        lat: 46.515998687649976,  
-        lng: 9.865724417607687,
-        embedCode: '<blockquote class="tiktok-embed" cite="https://www.tiktok.com/@engadin/video/7296909946724076833" data-video-id="7296909946724076833" style="max-width: 605px;min-width: 325px;" > <section> <a target="_blank" title="@engadin" href="https://www.tiktok.com/@engadin?refer=embed">@engadin</a> Bauernhof im Engadin 🐄🏔️ &gt;&gt;&gt;&gt;&gt; <a title="engadin" target="_blank" href="https://www.tiktok.com/tag/engadin?refer=embed">#engadin</a> <a title="bauernhof" target="_blank" href="https://www.tiktok.com/tag/bauernhof?refer=embed">#bauernhof</a> <a title="bauer" target="_blank" href="https://www.tiktok.com/tag/bauer?refer=embed">#bauer</a> <a target="_blank" title="♬ original sound - Engadin" href="https://www.tiktok.com/music/original-sound-7296909976926014241?refer=embed">♬ original sound - Engadin</a> </section> </blockquote> <script async src="https://www.tiktok.com/embed.js"></script>',
+        lat: 46.4909,  
+        lng: 9.8499,
+        embedCode: '<blockquote class="tiktok-embed" cite="https://www.tiktok.com/@engadin/video/7298398297050352929" data-video-id="7298398297050352929" style="max-width: 605px;min-width: 325px;" > <section> <a target="_blank" title="@engadin" href="https://www.tiktok.com/@engadin?refer=embed">@engadin</a> Mit wem möchtisch du go Herbstwandere? 🍂🧡 <a title="engadin" target="_blank" href="https://www.tiktok.com/tag/engadin?refer=embed">#engadin</a> <a title="schweiz" target="_blank" href="https://www.tiktok.com/tag/schweiz?refer=embed">#schweiz</a> <a title="herbst" target="_blank" href="https://www.tiktok.com/tag/herbst?refer=embed">#herbst</a> <a title="wandern" target="_blank" href="https://www.tiktok.com/tag/wandern?refer=embed">#wandern</a> <a title="fd" target="_blank" href="https://www.tiktok.com/tag/fd?refer=embed">#fd</a> <a title="fürdich" target="_blank" href="https://www.tiktok.com/tag/f%C3%BCrdich?refer=embed">#fürdich</a> <a target="_blank" title="♬ Originalton - Engadin" href="https://www.tiktok.com/music/Originalton-7298398306227931937?refer=embed">♬ Originalton - Engadin</a> </section> </blockquote> <script async src="https://www.tiktok.com/embed.js"></script>',
     },
     {
-        lat: 46.4986432007939, 
-        lng: 9.752693673770116, 
-        embedCode: '<blockquote class="tiktok-embed" cite="https://www.tiktok.com/@engadin/video/7289101918087941409" data-video-id="7289101918087941409" style="max-width: 605px;min-width: 325px;" > <section> <a target="_blank" title="@engadin" href="https://www.tiktok.com/@engadin?refer=embed">@engadin</a> Colorful Engadin🌈🫶 <a title="engadin" target="_blank" href="https://www.tiktok.com/tag/engadin?refer=embed">#engadin</a> <a title="trend" target="_blank" href="https://www.tiktok.com/tag/trend?refer=embed">#trend</a> <a title="fyp" target="_blank" href="https://www.tiktok.com/tag/fyp?refer=embed">#fyp</a> <a target="_blank" title="♬ original sound - Engadin" href="https://www.tiktok.com/music/original-sound-7289101931417455392?refer=embed">♬ original sound - Engadin</a> </section> </blockquote> <script async src="https://www.tiktok.com/embed.js"></script>',
+        lat: 46.4931, 
+        lng: 9.9058, 
+        embedCode: '<blockquote class="tiktok-embed" cite="https://www.tiktok.com/@engadin/video/7221189817856560410" data-video-id="7221189817856560410" style="max-width: 605px;min-width: 325px;" > <section> <a target="_blank" title="@engadin" href="https://www.tiktok.com/@engadin?refer=embed">@engadin</a> Was war dein Winter Highlight?🥶 👇🏽🧗🏻‍♀️<a title="eisklettern" target="_blank" href="https://www.tiktok.com/tag/eisklettern?refer=embed">#eisklettern</a> <a title="engadin" target="_blank" href="https://www.tiktok.com/tag/engadin?refer=embed">#engadin</a> <a title="pontresina" target="_blank" href="https://www.tiktok.com/tag/pontresina?refer=embed">#pontresina</a> <a title="actionsport" target="_blank" href="https://www.tiktok.com/tag/actionsport?refer=embed">#actionsport</a> <a title="wintersport" target="_blank" href="https://www.tiktok.com/tag/wintersport?refer=embed">#wintersport</a> <a target="_blank" title="♬ original sound - Engadin" href="https://www.tiktok.com/music/original-sound-7221189798102272794?refer=embed">♬ original sound - Engadin</a> </section> </blockquote> <script async src="https://www.tiktok.com/embed.js"></script>',
     }
+    
 
 ];
 
@@ -51,3 +52,20 @@ tiktokVideos.forEach(function(video) {
 });
 
 
+
+
+// Menu
+const menu = document.querySelector('.menu');
+
+// Get the burger icon element
+const burgerIcon = document.querySelector('.burger-icon');
+
+// Add an event listener to the burger icon for click events
+burgerIcon.addEventListener('click', function() {
+    // Toggle the display of the menu
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+});
